3ad30bc1d6e3958440bbd57c16c9fa4fff47c5d6
Kiran Mahn CS2021 <kiranmahn@gmail.com>
2024-03-03 13:40:56 +0000

    add dynamic data

--- GuideScreen.js
+++ GuideScreen.js
@@ -1,4 +1,4 @@
-import { Text, TouchableOpacity, View, Image } from "react-native";
+import { Text, TouchableOpacity, View, Image, ScrollView } from "react-native";
 import { getArticlesData } from "./Requests";
 import { Component } from "react";
 /* Contains a list of mindfulness mini articles */
@@ -8,56 +8,37 @@ import { Component } from "react";
 const GuideScreen = ({navigation, route}) => {
 
   // get data for all articles
-  // const articles = getArticlesData();
+  let data = getArticlesData();
+
+  console.log(data.articles);
+
+  let listComponents = [];
 
 
+  for(let i = 0; i < data.articles.length; i++) {
+    console.log(data.articles);
+
+    listComponents.push(
+          <TouchableOpacity key={data.articles[i]["title"]} style={{width: '100%', height: '20%', backgroundColor: 'rgba(202, 227,248, 0.5)', margin: 5, padding: 5, display:'flex', justifyContent: 'flex-start', alignItems: 'center', flexDirection: 'row', borderRadius: 15}}>
+            <View>
+              <Image source={require('./assets/bookIcon.png')} style={{alignSelf: 'center', margin: 10,}}/>
+            </View>
+            <View style={{flexDirection: 'col', width: '60%', height: '50%'}}>
+              <Text style={{fontSize: 20, margin: 10, fontWeight: 'bold'}}>{data.articles[i]["title"]}</Text>
+              <Text style={{fontSize: 15, margin: 5, flex: 1, flexWrap: 'wrap'}}>{data.articles[i]["shortDesc"]}</Text>
+            </View>
+            <Text style={{margin: 15, fontSize: 30, position: 'absolute', right: 5}}>&rarr;</Text>
+          </TouchableOpacity>
+    );
+  }
   
   return (
     <View style={{width: '100%', height: '100%', backgroundColor: 'white',}}>
       <Text style={{alignSelf: 'center', margin: '5%', fontSize:30, fontWeight:'bold'}}>Guide to mindfulness</Text>
-      <View id="ArticleBtnContainer" style={{display:'flex', justifyContent:'space-evenly', flexDirection:'column', alignItems: 'center', height: '70%', width: '100%', padding: 10}}>
-        <TouchableOpacity style={{width: '100%', height: '20%', backgroundColor: 'aliceblue', margin: 3, padding: 5, display:'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'row'}}>
-          <View>
-            <Image source={require('./assets/chatIcon.png')} style={{alignSelf: 'center', margin: 20,}}/>
-          </View>
-          <View>
-            <Text style={{fontSize: 20, margin: 10, fontWeight: 'bold'}}>Verbal Meditation</Text>
-            <Text style={{fontSize: 15, margin: 10}}>Out loud meditation</Text>
-          </View>
-          <Text style={{margin: 15, fontSize: 30}}>&rarr;</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={{width: '100%', height: '20%', backgroundColor: 'aliceblue', margin: 3, padding: 5, display:'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'row'}}>
-          <View>
-            <Image source={require('./assets/chatIcon.png')} style={{alignSelf: 'center', margin: 20,}}/>
-          </View>
-          <View>
-            <Text style={{fontSize: 20, margin: 10, fontWeight: 'bold'}}>Verbal Meditation</Text>
-            <Text style={{fontSize: 15, margin: 10}}>Out loud meditation</Text>
-          </View>
-          <Text style={{margin: 15, fontSize: 30}}>&rarr;</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={{width: '100%', height: '20%', backgroundColor: 'aliceblue', margin: 3, padding: 5, display:'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'row'}}>
-          <View>
-            <Image source={require('./assets/chatIcon.png')} style={{alignSelf: 'center', margin: 20,}}/>
-          </View>
-          <View>
-            <Text style={{fontSize: 20, margin: 10, fontWeight: 'bold'}}>Verbal Meditation</Text>
-            <Text style={{fontSize: 15, margin: 10}}>Out loud meditation</Text>
-          </View>
-          <Text style={{margin: 15, fontSize: 30}}>&rarr;</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={{width: '100%', height: '20%', backgroundColor: 'aliceblue', margin: 3, padding: 5, display:'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'row'}}>
-          <View>
-            <Image source={require('./assets/chatIcon.png')} style={{alignSelf: 'center', margin: 20,}}/>
-          </View>
-          <View>
-            <Text style={{fontSize: 20, margin: 10, fontWeight: 'bold'}}>Verbal Meditation</Text>
-            <Text style={{fontSize: 15, margin: 10}}>Out loud meditation</Text>
-          </View>
-          <Text style={{margin: 15, fontSize: 30}}>&rarr;</Text>
-        </TouchableOpacity>
+      <ScrollView id="ArticleBtnContainer" style={{display:'flex', flexDirection:'column', height: '80%', width: '100%', padding: 10, overflow: 'scroll'}} contentContainerStyle={{alignItems: 'center', justifyContent:'space-between', flexGrow: 1}}>
+        {listComponents}
         
-      </View>
+      </ScrollView>
     </View>
     
   );
--- Requests.js
+++ Requests.js
@@ -1,24 +1,60 @@
 
 const getArticlesData = () => {
-    return (
-            [ 
+    let data = { 
+        articles: [ 
                 {
                     "title": 'Verbal Meditation',
-                    "image": 'chatIcon.png', // find better way to store images?
+                    "image": './assets/chatIcon.png', // find better way to store images?
                     "shortDesc": 'Out loud meditation', 
-                    "textBody": 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metusPellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus', 
+                    "textBody": "textBody",
                     "id": '0'
                 },
                 {
                     "title": 'Thoughtfullness',
-                    "image": 'bookIcon.png', // find better way to store images?
+                    "image": './assets/bookIcon.png', // find better way to store images?
                     "shortDesc": 'Learn to be thoughtful towards yourself and others', 
-                    "textBody": 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metusPellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus', 
+                    "textBody": "textBody",
+                    "id": '0'
+                },
+                {
+                    "title": 'Breathing Excercises',
+                    "image": './assets/chatIcon.png', // find better way to store images?
+                    "shortDesc": 'Work on your breathwork to reduce stress', 
+                    "textBody": "textBody",
+                    "id": '0'
+                },
+                {
+                    "title": 'Thoughts and Feelings',
+                    "image": './assets/bookIcon.png', // find better way to store images?
+                    "shortDesc": 'Detach thoughts from emotions', 
+                    "textBody": "textBody",
+                    "id": '0'
+                },
+                {
+                    "title": 'Movement',
+                    "image": './assets/chatIcon.png', // find better way to store images?
+                    "shortDesc": 'Strengthen the mind-body connection', 
+                    "textBody": "textBody",
+                    "id": '0'
+                },
+                {
+                    "title": 'Verbal Meditation 2',
+                    "image": './assets/bookIcon.png', // find better way to store images?
+                    "shortDesc": 'Detach thoughts from emotions', 
+                    "textBody": "textBody",
+                    "id": '0'
+                },
+                {
+                    "title": 'Thoughts and Feelings 2',
+                    "image": './assets/chatIcon.png', // find better way to store images?
+                    "shortDesc": 'Detach thoughts from emotions', 
+                    "textBody": "textBody",
                     "id": '0'
                 },
             ]
         
-    );
+        }
+        return data;
 }
 
 const Requests = () => {
